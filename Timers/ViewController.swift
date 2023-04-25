6ed279802f34fde9256674be21cd62d9e0e84d9f
jrcrisp <jrcrisp@ncsu.edu>
2023-04-25 08:33:40 -0400

    Initial Commit

---
+++ Timers/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Timers
+//
+//  Created by James Crisp on 4/25/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
